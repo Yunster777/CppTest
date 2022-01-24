44d0a5be88ececf4cd3d363ee7b33db8972b52ec
Yunster777 <kysung14@gmail.com>
2022-01-24 22:21:36 +0900

    ~ Priority Queue

--- CppTest.cpp
+++ CppTest.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 
 // 함수 정의
@@ -10,6 +11,15 @@ void Substring_Test();
 void MakeVector();
 void MakeArray();
 void ReverseStr();
+void MakeMap();
+void MakeSet();
+void MakeMultiSet();
+void MakeStack();
+void MakeQueue();
+void MakeDeque();
+void MakePriorityQueue();
+void MakePriorityQueue2();
+void MakePriorityQueue3();
 
 int main()
 {
@@ -17,9 +27,213 @@ int main()
     //PairNTuple();
 	//MakeVector();
 	//MakeArray();
-	ReverseStr();
+	//ReverseStr();
+	//MakeMap();
+	//MakeSet();
+	//MakeMultiSet();
+	//MakeStack();
+	//MakeQueue();
+	//MakeDeque();
+	//MakePriorityQueue();
+	//MakePriorityQueue2();
+	MakePriorityQueue3();
 
     return 0;
+}
+
+void MakePriorityQueue3() {
+	// 우선순위 queue 커스텀 정렬
+	struct Point
+	{
+		int y, x;
+	};
+
+	struct cmp {
+		bool operator() (Point a, Point b) {
+			return a.x < b.x;
+		}
+	};
+
+	priority_queue<Point, vector<Point>, cmp> pq;
+
+	pq.push({ 1, 1 });
+	pq.push({ 2, 2 });
+	pq.push({ 3, 3 });
+	pq.push({ 4, 4 });
+	pq.push({ 5, 5 });
+	pq.push({ 6, 6 });
+
+	cout << pq.top().x << "\n";
+}
+
+void MakePriorityQueue2() {
+	// 우선순위 queue에 다른 자료구조 넣기
+	struct Point
+	{
+		int y, x;
+		Point(int y, int x) : y(y), x(x) {}
+		Point() { y = -1; x = -1; }
+		bool operator < (const Point& a) const {
+			return x < a.x;
+		}
+	};
+
+	priority_queue<Point> pq;
+
+	pq.push({ 1, 1 });
+	pq.push({ 2, 2 });
+	pq.push({ 3, 3 });
+	pq.push({ 4, 4 });
+	pq.push({ 5, 5 });
+	pq.push({ 6, 6 });
+
+	cout << pq.top().x << "\n";
+}
+
+void MakePriorityQueue() {
+	// 우선순위 queue. 내부 구조는 heap으로 구현되어 있으며, 주로 다익스트라, 그리디 등에 사용
+	// greater : 오름차순, less : 내림차순
+	//priority_queue<int, vector<int>, greater<int>> pq; // 오름차순
+	priority_queue<int, vector<int>, less<int>> pq; // 내림차순
+
+	pq.push(5);
+	pq.push(4);
+	pq.push(3);
+	pq.push(2);
+	pq.push(1);
+
+	cout << pq.top() << "\n";
+}
+
+void MakeDeque() {
+	// Deque : queue랑 비슷하지만 앞/뒤로 넣고 뺄 수 있음
+	deque<int> dq;
+	dq.push_front(1);
+	dq.push_back(2);
+	dq.push_back(3);
+
+	cout << dq.front() << "\n";
+	cout << dq.back() << "\n";
+	cout << dq.size() << "\n";
+
+	dq.pop_back();
+	dq.pop_front();
+
+	cout << dq.front() << "\n";
+	cout << dq.back() << "\n";
+	cout << dq.size() << "\n";
+
+}
+
+void MakeQueue() {
+	// FIFO
+	queue<int> q;
+	
+	q.push(1);
+	cout << q.front() << "\n";
+
+	q.pop();
+	cout << q.size() << "\n";
+}
+
+void MakeStack() {
+	// LIFO
+	stack<string> stk;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	stk.push("엄");
+	stk.push("준");
+	stk.push("식");
+	stk.push("화");
+	stk.push("이");
+	stk.push("팅");
+
+	while (stk.size()) {
+		cout << stk.top() << "\n";
+		stk.pop();
+	}
+}
+
+void MakeMultiSet() {
+	// 중복되는 원소도 집어넣을 수 있는 자료구조.
+	// key, value 형태로 집어넣을 필요도 없고, 넣으면 자동으로 정렬됨
+	multiset<int> s;
+
+	s.insert(12);
+	s.insert(10);
+	s.insert(2);
+	s.insert(10);
+	s.insert(90);
+	s.insert(85);
+	s.insert(45);
+
+	cout << "Multiset elements after sort\n";
+	for (auto it = s.begin(); it != s.end(); it++)
+		cout << *it << ' ';
+	cout << "\n";
+
+	auto it1 = s.find(10);
+	auto it2 = s.find(90);
+
+	// elements from 10 to elements before 90
+	// erased
+	s.erase(it1, it2);
+
+	cout << "Multiset Elements after erase:\n";
+	for (auto it = s.begin(); it != s.end(); it++)
+		cout << (*it) << ' ';
+	s.erase(s.begin());
+	cout << "\n";
+	for (auto it = s.begin(); it != s.end(); it++)
+		cout << (*it) << ' ';
+}
+
+void MakeSet() {
+	// 중복되는 요소는 없고 오로지 유니크한 값만 저장해버리는 자료구조
+	set<pair<string, int>> _set;
+
+	_set.insert({ "test", 1 });
+	_set.insert({ "test", 1 });
+	_set.insert({ "test", 1 });
+	_set.insert({ "test", 1 });
+
+	cout << _set.size() << "\n";
+}
+
+void MakeMap() {
+	int v[10];
+	unordered_map<string, int> umap;
+	
+	// 이렇게 넣기도 가능
+	umap.insert({ "test1", 1 });
+
+	// 이렇게 넣을수도 있음
+	umap.emplace("test5", 5);
+
+	// 또한 이렇게 변경도 가능, 추가할 수도 있음. 아래를 권장
+	umap["test1"] = 4;
+
+	for (auto element : umap) {
+		cout << element.first << " :: " << element.second << '\n';
+	}
+
+	// map의 find 메소드는 찾지 못하면 end() 이터레이터를 반환
+	auto search = umap.find("test1");
+	if (search != umap.end()) {
+		cout << "found :" << search->first << " " << (*search).second << '\n';
+	}
+	else {
+		cout << "not found.." << '\n';
+	}
+
+	// 이런 식으로 바로 int형을 증가시킬 수 있음
+	umap["test1"]++;
+	cout << umap["test1"] << '\n';
+
+	cout << umap.size() << '\n';
+	umap.erase("test1");
+	cout << umap.size() << '\n';
 }
 
 void ReverseStr() {
